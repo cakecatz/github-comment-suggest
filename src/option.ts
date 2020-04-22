381d9407fbba51ef4bb34d6c05b75ca34ec32ac2
Ryo Narita <mail.ryonarita@gmail.com>
2020-04-23 01:24:06 +0900

    Show error message if option json is invalid

--- src/option.ts
+++ src/option.ts
@@ -4,17 +4,38 @@ function getCommandListTextArea(): HTMLTextAreaElement {
   return <HTMLTextAreaElement>document.getElementById("suggest-list");
 }
 
+function validateJSON(option: string): boolean {
+  let parsedData;
+  try {
+    parsedData = JSON.parse(option);
+  } catch {
+    return false;
+  }
+
+  return Array.isArray(parsedData);
+}
+
+function showStatusMessage(message: string) {
+  const status = document.getElementById("status");
+  status.textContent = message;
+  setTimeout(function() {
+    status.textContent = "";
+  }, 2000);
+}
+
 async function saveOptions() {
   const t = getCommandListTextArea();
+
+  if (validateJSON(t.value) === false) {
+    showStatusMessage("JSON Parse error");
+    return;
+  }
+
   await optionsStorage.set({
     commandList: t.value
   });
 
-  const status = document.getElementById("status");
-  status.textContent = "Options saved.";
-  setTimeout(function() {
-    status.textContent = "";
-  }, 750);
+  showStatusMessage("Options saved");
 }
 
 async function restoreOptions() {
